b58c91047d966f445c4efaf685bc65c489852c16
phamthaihoang1234 <phamthaihoang1412@gmail.com>
2021-03-16 09:21:49 +0700

    change_b

--- src/Check.java
+++ src/Check.java
@@ -1,6 +1,8 @@
 public class Check {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 111;
+        int b = 13;
         System.out.println(a);
+        System.out.println(b);
     }
 }
