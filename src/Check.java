a82e361ccbe587aba3ab4350b2983dcfd4190d53
phamthaihoang1234 <phamthaihoang1412@gmail.com>
2021-03-16 09:46:43 +0700

    change_branch_hoang

--- src/Check.java
+++ src/Check.java
@@ -2,7 +2,10 @@ public class Check {
     public static void main(String[] args) {
         int a = 111;
         int b = 13;
+        int c = 12;
         System.out.println(a);
         System.out.println(b);
+        System.out.println(c);
+        System.out.println(a+b+c);
     }
 }
